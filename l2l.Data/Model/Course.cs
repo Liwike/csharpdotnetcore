445302d6ccd5a0c0c04f1d05310aecab359c669c
Liwike73 <liwike73@gmail.com>
2020-08-02 23:04:41 +0200

    Antipattern kirtása

--- l2l.Data/Model/Course.cs
+++ l2l.Data/Model/Course.cs
@@ -2,41 +2,11 @@ using System;
 
 namespace l2l.Data.Model
 {
-    public class Course : IEquatable<Course>
+    public class Course
     {
-        public Course()
-        {
-        }
 
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public override bool Equals(object? obj)
-        {
-            return Equals(obj as Course);
-        }
-
-        public bool Equals(Course c)
-        {
-            if (null == c || Id != c.Id || Name != c.Name)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            //túlcsordulás ellenőrző
-            unchecked
-            {
-                var hash = 27;
-                hash = (13 * hash) + Id.GetHashCode();
-                hash = (13 * hash) + Name.GetHashCode();
-                return hash;
-            }
-
-        }
     }
-
 }
